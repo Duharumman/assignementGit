9275818c073c9be4dbf3e155a423319414664134
Duha <duha.abu99@gmail.com>
2021-01-08 03:58:29 +0200

    hello

--- src/Mian.java
+++ src/Mian.java
@@ -3,6 +3,7 @@ public class Mian {
         System.out.println("Hello to our project xD");
         System.out.println("We are working ):");
         System.out.println("Atypon treaning (:");
+        System.out.println("hello world");
 
     }
 }
