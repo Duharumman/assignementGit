e95ab8559d9c731b3df59dbc267d4e8f61a105d8
Duha <duha.abu99@gmail.com>
2021-01-08 03:42:55 +0200

    ATYOPN

--- src/Mian.java
+++ src/Mian.java
@@ -2,6 +2,7 @@ public class Mian {
     public static void main(String[] args) {
         System.out.println("Hello to our project xD");
         System.out.println("We are working ):");
+        System.out.println("Atypon treaning (:");
 
     }
 }
